4685be7e31290c9a5ee50ceaf4ee111e46582840
Mateusz Kuba <mateusz.kuba@onet.eu>
2019-04-09 23:44:45 +0200

    change

--- WebPagesDownload/serializers.py
+++ WebPagesDownload/serializers.py
@@ -1,11 +1,22 @@
 from WebPagesDownload.models import Download, WebImage, WebText
 from rest_framework import serializers
+import base64
+from django.core.files import File
 
 
 class WebImageSerializer(serializers.ModelSerializer):
+    data = serializers.SerializerMethodField()
+
     class Meta:
         model = WebImage
         fields = ('id','data','download')
+
+    def get_data(self, obj):
+        f = open(obj.image_file.path, 'rb')
+        image = File(f)
+        data = base64.b64encode(image.read())
+        f.close()
+        return data
 
 
 class WebTextSerializer(serializers.ModelSerializer):
